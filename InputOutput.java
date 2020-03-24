f9462eda61b14cfd9b4a7f7b0dc43d170ba658dc
ACER <mrosyadi23@outlook.com>
2020-03-25 00:07:32 +0800

    Update InputOutput.java

--- InputOutput.java
+++ InputOutput.java
@@ -4,9 +4,20 @@ public class InputOutput{
  public static void main(String [] args){
    Scanner scanner = new Scanner(System.in);
     String nama;
+	int jumlahSks;
+	double IPK;
+
 	System.out.print("Masukkan nama anda: ");
 	nama = scanner.nextLine();
-
 	System.out.println("Hai, "+nama);
+
+	System.out.print("Masukkan SKS yang sudah ditempuh: ");
+	jumlahSks = scanner.nextLine();
+
+	System.out.print("Masukkan IPK Sementara : ");
+	IPK = scanner.Double();
+
+	double jumlah = IPK Sementara : ");
+	System.out.print("Total Nilai = "+jumlah;
 	}
 }
