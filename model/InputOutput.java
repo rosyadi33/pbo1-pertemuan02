36cc7e3a5c57f2e8e17f5b64039044abb32e8d83
ACER <mrosyadi23@outlook.com>
2020-03-24 23:22:51 +0800

    Buat Output

--- model/InputOutput.java
+++ model/InputOutput.java
@@ -5,9 +5,13 @@ public class InputOutput{
 	Scanner scanner = new Scanner(System.in);
 	String nama;
 	System.out.print("Masukkan nama anda: ");
+<<<<<<< HEAD
 	nama = scanner.nextline();
 
 	System.out.println("Hai, "+nama);
+=======
+	nama =  scanner.nextline();
+>>>>>>> parent of ddb83f5... Buat Output
 
   }
 }
