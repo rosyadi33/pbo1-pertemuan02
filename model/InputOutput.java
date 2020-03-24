04bb8008f82c233bb709add7c4a1a57b7924b332
ACER <mrosyadi23@outlook.com>
2020-03-24 23:20:14 +0800

    Buat Output

--- model/InputOutput.java
+++ model/InputOutput.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 public class InputOutput{
-  public static void main(String []args);
+  public static void main(String []args){
 	Scanner scanner = new Scanner(System.in);
 	String nama;
 	System.out.print("Masukkan nama anda: ");
-	nama =  scanner.nextline();
+	nama = scanner.nextline();
 
-	System.out.println(Hai, "+nama);
+	System.out.println("Hai, "+nama);
 
   }
 }
